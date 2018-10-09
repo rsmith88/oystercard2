4ca5817f72b1293fb2ba7e187003d6b8887eece1
Becka <rebdok@hotmail.com>
2018-10-09 15:27:23 +0100

    added in journey method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,25 @@
 class OysterCard
 
+  MAX_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-    raise "Maximum limit exceeded" if @balance + amount > 90
+    raise "Maximum limit (£#{MAX_BALANCE}) exceeded" if @balance + amount > 90
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey? 
+    @in_journey
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,17 @@ describe OysterCard do
   end
 
   it "raise an error if maximum limit is exceeded" do
-    expect{subject.top_up(100)}.to raise_error "Maximum limit exceeded"
+    max_balance = described_class::MAX_BALANCE
+    expect{subject.top_up(max_balance + 1)}
+    .to raise_error "Maximum limit (£#{max_balance}) exceeded"
+  end
+
+  it 'deducts from balance' do
+    expect{subject.deduct(5)}.to change{subject.balance}.by -5
+  end
+
+  it 'newly created card in not in journey' do
+    expect(subject).not_to be_in_journey
   end
 
 end
